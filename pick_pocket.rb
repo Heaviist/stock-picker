f7ae5291125f16de992d01b5be63401ae8b3a194
Hennie Bruurmijn <hlm.bruurmijn@gmail.com>
2022-05-09 23:29:39 +0700

    Write stock_picker method
    Could probably be written prettier. Also not sure about all possible edge cases

--- pick_pocket.rb
+++ pick_pocket.rb
@@ -1,4 +1,23 @@
 # frozen_string_literal: true
 
 # stock-picker
-def stock_picker; end
+def stock_picker(prices, buy_sell = [0, 0], buy_index = 0, profit = 0)
+  buy_price = prices[buy_index]
+  prices.each_with_index do |price, index|
+    if price < buy_price
+      buy_price = price
+      buy_index = index
+    end
+    if price - buy_price > profit
+      profit = price - buy_price
+      buy_sell = [buy_index, index]
+    end
+  end
+  buy_sell
+end
+
+prices_by_day = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20].shuffle
+
+p prices_by_day
+
+p stock_picker(prices_by_day)
